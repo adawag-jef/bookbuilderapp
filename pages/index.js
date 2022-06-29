00e5c45576a39d0f821662540054125ecfea3323
jeff <jeff.salarium@gmail.com>
2022-06-29 16:41:54 +0800

    feat: Header component

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
-import Head from 'next/head';
+import Head from "next/head";
+import Header from "../components/Header";
 
 const Index = () => (
-  <div style={{ padding: '10px 45px' }}>
+  <div style={{ padding: "10px 45px" }}>
     <Head>
       <title>Index page</title>
-      <meta name="description" content="This is the description of the Index page" />
+      <meta
+        name="description"
+        content="This is the description of the Index page"
+      />
     </Head>
+    <Header />
     <p>Content on Index page</p>
   </div>
 );
